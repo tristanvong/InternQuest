a0f114cb8c3e57d895ad50ef1f80823dd1bb6418
tristanvong <146710405+tristanvong@users.noreply.github.com>
2024-11-30 15:03:46 +0100

    Laatste test 3

--- src/main/java/be/ehb/tristan/javaadvanced/internquest/InternQuestApplication.java
+++ src/main/java/be/ehb/tristan/javaadvanced/internquest/InternQuestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InternQuestApplication {
-//iets extra e
+//iets extra e e
     public static void main(String[] args) {
         SpringApplication.run(InternQuestApplication.class, args);
     }
