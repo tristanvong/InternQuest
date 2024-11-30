458983a044328f7f68fe63bf8ead97138d4b7dff
tristanvong <146710405+tristanvong@users.noreply.github.com>
2024-11-30 14:40:50 +0100

    Initial commit

---
+++ src/main/java/be/ehb/tristan/javaadvanced/internquest/InternQuestApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.tristan.javaadvanced.internquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternQuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternQuestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/tristan/javaadvanced/internquest/InternQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.tristan.javaadvanced.internquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternQuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
